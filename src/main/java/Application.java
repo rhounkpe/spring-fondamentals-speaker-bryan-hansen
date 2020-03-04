9840f0165def281de87aed57a55af85dca71891b
Rufin Hounkpe <rhounkpe@gmail.com>
2020-03-04 12:14:44 +0100

    Setteur injection

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,13 @@
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
-        SpeakerService speakerService = new SpeakerServiceImpl();
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        // SpeakerService speakerService = new SpeakerServiceImpl();
+        SpeakerService speakerService = context.getBean("speakerService", SpeakerService.class);
 
         System.out.println(speakerService.findAll().get(0).getFirstName());
     }
--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -7,10 +7,14 @@ import com.pluralsight.repository.SpeakerRepository;
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
-    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+    private SpeakerRepository repository;
 
     @Override
     public List<Speaker> findAll() {
         return repository.findAll();
+    }
+
+    public void setRepository(SpeakerRepository repository) {
+        this.repository = repository;
     }
 }
