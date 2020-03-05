e22d298c0242f17ee9b49dce4b7ca8247b32370c
Rufin Hounkpe <rhounkpe@gmail.com>
2020-03-05 09:03:50 +0100

    Adding Bean. Setter Injection

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,13 @@
 import com.pluralsight.service.SpeakerService;
-import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
-        SpeakerService speakerService = new SpeakerServiceImpl();
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // SpeakerService speakerService = new SpeakerServiceImpl();
+        SpeakerService speakerService = applicationContext.getBean("speakerService", SpeakerService.class);
 
         System.out.println(speakerService.findAll().get(0).getFirstName());
     }
--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -1,16 +1,19 @@
 package com.pluralsight.service;
 
 import com.pluralsight.model.Speaker;
-import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
 
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
-    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+    private SpeakerRepository repository;
 
     @Override
     public List<Speaker> findAll() {
         return repository.findAll();
+    }
+
+    public void setSpeakerRepository(SpeakerRepository repository) {
+        this.repository = repository;
     }
 }
